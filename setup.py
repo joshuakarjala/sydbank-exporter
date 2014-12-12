d594f543e3ef8e8d929cabf659e8c756aba0837e
Joshua Karjala-Svendsen <joshua@founders.as>
2014-12-12 12:39:35 +0100

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(
-    name='sydbank-exporter  ',
+    name='sydbank-exporter',
     version='0.1.0',
     description='Create a CSV file that can be read by Sydbank',
     author='Joshua Karjala-Svendsen',
